5cc2276387412fd05e96f693eda89caed46752a2
Raghav45 <gopalakrishnan.r2@gmail.com>
2021-03-25 22:36:29 -0400

    separate menu item from menu

--- src/screens/other/MenuScreen.js
+++ src/screens/other/MenuScreen.js
@@ -1,7 +1,9 @@
+import { useLinkProps } from '@react-navigation/native';
 import React, { useCallback } from 'react';
 import { StyleSheet,StatusBar, Text, View,SectionList } from 'react-native';
-import { ListItem, Avatar } from 'react-native-elements';
 import { FAB } from 'react-native-paper';
+import MenuItem from '../../../components/3/MenuItem'
+import { useNavigation } from '@react-navigation/native';
 
 
 const data = [ 
@@ -67,32 +69,14 @@ const data = [
 
 export default MenuScreen = ({navigation}) => { 
 
-  const renderItem = useCallback(({ item }) => (
-    <ListItem onPress={() => navigation.navigate("ItemScreen")} containerStyle = {{marginHorizontal:16,marginBottom:'2%', paddingLeft: '3%',borderRadius:6,height:70,backgroundColor:'#c9e9ff'}}>
-      
-      <ListItem.Content style={{flex:0.3}}>
-        <Avatar 
-
-            source={
-              require('../../assets/burger.jpg')
-            }
-            >
-          
-        </Avatar>
-      </ListItem.Content>
-      <ListItem.Content style = {{alignItems:'flex-start'}}>
-        <ListItem.Title style = {{fontSize:19}}>{item.name}</ListItem.Title>
-        <ListItem.Subtitle>{item.desc}</ListItem.Subtitle>
-      </ListItem.Content>
   
-        <ListItem.Content>
-            
-            <ListItem.Subtitle style = {{fontSize:16,alignSelf:'flex-end'}}>{item.price}</ListItem.Subtitle>
-        </ListItem.Content>
-      
-      
-    </ListItem>
-  ),[]);
+  const renderItem =({item}) => {
+
+    
+    return(
+    <MenuItem item={item}/>
+    );
+  }
 
   const keyExtractor= useCallback((item) => item.name,[]);
 
