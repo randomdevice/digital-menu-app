906d2e3abbd4defc4f1c572430e44c9a5ff95baa
directkunj6 <78246320+directkunj6@users.noreply.github.com>
2021-04-03 14:24:24 -0400

    Updated comp 17 to correct format, changed name

---
+++ components/other/17/Component17Container.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Image, SafeAreaView,StyleSheet} from "react-native";
+const itemImage= 'https://picsum.photos/200/'   //to be changed to firebase url
+
+export default Component17Container = () => {  
+ return (
+   <Component17/>
+    )
+}
+
+const Component17 =() => {
+  return (
+    <SafeAreaView>
+      <Image style={styles.container}
+       source={{uri:itemImage}} 
+       />
+    </SafeAreaView>
+  );
+}
+const styles= StyleSheet.create({
+  container:{
+    width:400,
+    height:180
+  }
+})
