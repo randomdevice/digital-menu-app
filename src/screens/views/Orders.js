ab88d2fafa28b34700fb14bee4d6494d2e2d6f93
suryanshpsingh <67669995+suryanshpsingh@users.noreply.github.com>
2021-04-08 19:43:55 -0400

    Update Orders.js

--- src/screens/views/Orders.js
+++ src/screens/views/Orders.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import {View, Text} from 'react-native'
+import Constants from 'expo-constants';
+import firebase from 'firebase'
 
 export class OrderDashboard extends Component {
     render() {
